a216cabb5d3141ad09c1efe1aa5380a96dbc46e8
mickuki321 <michal@zieglerlabs.com>
2021-01-25 13:32:56 +0100

    DEV-251 add script deploy AWS

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const github = require('@actions/github');
 
 (async () => { try {
     const tags = JSON.parse(core.getInput('tags'));
@@ -17,9 +16,8 @@ const github = require('@actions/github');
     await exec.exec('sudo unzip update-aws-ecs-service-linux-amd64.zip -d /usr/bin');
     await exec.exec('sudo chmod +x /usr/bin/update-aws-ecs-service');
 
-    console.log(`Install update-aws-ecs-service`);
-    await exec.exec('aws ecr get-login-password --region eu-central-1');
-    await exec.exec('docker login --username AWS --password-stdin 049470867734.dkr.ecr.eu-central-1.amazonaws.com');
+    console.log(`Login to AWS ECR`);
+    await exec.exec('aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 049470867734.dkr.ecr.eu-central-1.amazonaws.com');
 
     console.log(`Build the docker images with docker compose`);
     await exec.exec(buildCommand)
