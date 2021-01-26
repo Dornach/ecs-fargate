eb60bf6c2cd24fb11c3aa68c24e4805cd7fb1db2
mickuki321 <michal@zieglerlabs.com>
2021-01-26 11:40:58 +0100

    DEV-251 test add AWS script

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const io = require('@actions/io');
-const fs = require('file-system');
 
 (async () => { try {
     let myOutput = '';
@@ -17,24 +15,6 @@ const fs = require('file-system');
     const service = core.getInput('service');
     const cluster = core.getInput('cluster');
     const buildCommand = core.getInput('build-command');
-    const awsAccessKeyId = core.getInput('aws-access-key-id');
-    const awsSecretAccessKey = core.getInput('aws-secret-access-key');
-    const awsRegion = core.getInput('aws-region');
-
-    console.log(`Configure AWS credentials`);
-    // core.exportVariable('AWS_ACCESS_KEY_ID', awsAccessKeyId);
-    // core.exportVariable('AWS_SECRET_ACCESS_KEY', awsSecretAccessKey);
-    // core.exportVariable('AWS_REGION', awsRegion);
-    // core.exportVariable('AWS_DEFAULT_REGION', awsRegion);
-
-    const credentials=`[default]\naws_access_key_id = ${awsAccessKeyId}$\naws_secret_access_key = ${awsSecretAccessKey}`
-    const config=`[default]\nregion = ${awsRegion}$`
-    await exec.exec('ls -la /usr/local/bin')
-    await exec.exec('ls -la /usr/local/bin/aws')
-    await fs.appendFileSync('/usr/local/bin/aws/credentials', credentials,(err)=>console.log(err));
-    await exec.exec('ls -la /usr/local/bin/credentials')
-    await fs.appendFileSync('/usr/local/bin/aws/config', config,(err)=>console.log(err));
-
 
     console.log(`Install update-aws-ecs-service`);
     await exec.exec('wget https://github.com/Autodesk/go-awsecs/releases/download/v1.3.1/update-aws-ecs-service-linux-amd64.zip');
