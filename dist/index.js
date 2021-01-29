98ac8bda20e57ff2363e71ba34bcf50f6d25c9f4
mickuki321 <michal@zieglerlabs.com>
2021-01-29 11:09:19 +0100

    DEV-251 correction

--- dist/index.js
+++ dist/index.js
@@ -22,6 +22,7 @@ const exec = __nccwpck_require__(159);
     const service = core.getInput('service');
     const cluster = core.getInput('cluster');
     const buildCommand = core.getInput('build-command');
+    const awsRegion = core.getInput('aws-region');
     const ecr = core.getInput('ecr');
 
     await tags.map( val => {
@@ -37,21 +38,23 @@ const exec = __nccwpck_require__(159);
     await exec.exec('sudo unzip update-aws-ecs-service-linux-amd64.zip -d /usr/bin');
     await exec.exec('sudo chmod +x /usr/bin/update-aws-ecs-service');
 
-    console.log(`Login to AWS ECR`);
-    await exec.exec('aws ecr get-login-password --region eu-central-1','',options);
-    await exec.exec(`docker login --username AWS --password ${myOutput} ${ecr}`);
+    if(buildCommand) {
+        console.log(`Build the docker images with docker compose`);
+        await exec.exec(`docker-compose -f ${buildCommand} build`)
+    }
 
-    console.log(`Build the docker images with docker compose`);
-    await exec.exec(`docker-compose -f ${buildCommand} build`)
+    console.log(`Login to AWS ECR`);
+    await exec.exec(`aws ecr get-login-password --region ${awsRegion}`,'',options);
+    await exec.exec(`docker login --username AWS --password ${myOutput} ${ecr}`);
 
     let updateAWSCommand=`update-aws-ecs-service -cluster ${cluster} -service ${service} `
 
     for(let i=0;i<tags.length;i++) {
         const val = tags[i]
-        updateAWSCommand+=`-container-image ${val.containerImage}=${ecr}/${val.tag}:latest `
+        updateAWSCommand+=`-container-image ${val.containerImage}=${ecr}/${val.tag}:${val.env ? val.env : 'latest'} `
         console.log(`Tag ${val.tag}`)
-        await exec.exec(`docker tag ${val.imageName ? val.imageName : val.tag}:latest ${ecr}/${val.tag}:latest`)
-        await exec.exec(`docker push ${ecr}/${val.tag}:latest`)
+        await exec.exec(`docker tag ${val.imageName ? val.imageName : val.tag}:${val.env ? val.env : 'latest'} ${ecr}/${val.tag}:${val.env ? val.env : 'latest'}}`)
+        await exec.exec(`docker push ${ecr}/${val.tag}:${val.env ? val.env : 'latest'}`)
     }
 
     console.log(`Update an AWS ECS service with the new image`);
